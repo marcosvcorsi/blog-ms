266fab8e7a633609f5c4691b65d6da0643d7ea80
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-14 21:17:23 -0300

    refactor: improving logs

--- event-bus/src/server.js
+++ event-bus/src/server.js
@@ -25,35 +25,41 @@ const services = [
     port: 4002,
   },
   {
-    service: 'localhost',
-    port: 4003,
-  },
-  {
     service: 'moderation-service',
     port: 4004,
   }
 ]
+
+const dispatchEvent = async (event) => {
+  try {
+    const results = await Promise.allSettled(services.map(({ service, port }) => axios.post(`http://${service}:${port}/events`, event)));
+
+    results.forEach((result, index) => {
+      const { status } = result;
+  
+      const { service, port } = services[index];
+  
+      if(status === 'fulfilled') {
+        console.log(`Event publish at ${service}:${port}`)
+      } else {
+        console.log(`Event error at ${service}:${port}`)
+      }
+    });
+
+    console.log('Event dispatch successfully');
+  } catch(error) {
+    console.error('Error dispatching event', error);
+  }
+}
 
 app.post('/events', async (req, res) => {
   const event = req.body;
 
   events.push(event);
 
-  const results = await Promise.allSettled(services.map(({ service, port }) => axios.post(`http://${service}:${port}/events`, event)));
+  res.send();
 
-  results.forEach((result, index) => {
-    const { status } = result;
-
-    const { service, port } = services[index];
-
-    if(status === 'fulfilled') {
-      console.log(`Event publish at ${service}:${port}`)
-    } else {
-      console.log(`Event error at ${service}:${port}`)
-    }
-  });
-
-  return res.send();
+  dispatchEvent(event);
 });
 
 const port = process.env.PORT || 4005;
